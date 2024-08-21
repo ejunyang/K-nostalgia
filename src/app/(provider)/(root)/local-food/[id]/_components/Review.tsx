efcf8f112dff3c50338601bdaa6d86cb32cae473
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-08-21 16:05:19 +0900

    :adhesive_bandage: fix : 리뷰 소수점

--- src/app/(provider)/(root)/local-food/[id]/_components/Review.tsx
+++ src/app/(provider)/(root)/local-food/[id]/_components/Review.tsx
@@ -131,7 +131,8 @@ export const Review = ({ productId }: { productId: string }) => {
       <div className="border-b-2 border-[#F2F2F2] w-full">
         <div className="w-[235px] my-8 mx-auto flex items-center justify-center border border-[##E0E0E0] bg-white rounded-[8px] p-4">
           <p className="mr-4 font-semibold">
-            {averageRate} <span className="text-label-alternative"> / 5</span>
+            {averageRate.toFixed(1)}{' '}
+            <span className="text-label-alternative"> / 5</span>
           </p>
           <StarRating rating={averageRate} size={24} />
         </div>
